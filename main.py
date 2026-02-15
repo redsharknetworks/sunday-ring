07b0fd51c6476acbc4499277278ae3694b99abf2
redsharknetworks <redsharknetworks@gmail.com>
2026-02-15 16:35:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -182,7 +182,7 @@ def dashboard_html():
     </head>
     <body>
         <div class="header">
-            <img src="{{ url_for('static', filename='redshark.jpeg') }}" class="logo" />
+            <img src="{{ url_for('static', filename='redshark.png') }}" class="logo" />
             <h1>Malaysia Threat Intel Dashboard</h1>
         </div>
         <div class="email">Contact: darkgrid@redshark.my</div>
